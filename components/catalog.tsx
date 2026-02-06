7dd8414b1c75f949cacd51f468fffa1d2d17ec54
sherstnew <sherstnev.denis.v@gmail.com>
2026-02-06 11:45:58 +0300

    color fix

--- components/catalog.tsx
+++ components/catalog.tsx
@@ -175,7 +175,7 @@ export function Catalog({ tasks }: { tasks: TaskSchema[] }) {
         ) {
             return "text-red-600 bg-red-50 border-red-200";
         }
-        return "text-blue-600 bg-blue-50 border-blue-200";
+        return "text-green-600 bg-green-50 border-green-200";
     };
 
     return (
